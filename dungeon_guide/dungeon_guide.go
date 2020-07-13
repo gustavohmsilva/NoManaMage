b9c0a91b3085a2a820b6c5ab6995dc0d228071bf
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-07-13 22:32:37 +0100

    this is the commit of the seventh live on twitch teaching game development with the Defold game engine... Enjoy fam!

--- dungeon_guide/dungeon_guide.go
+++ dungeon_guide/dungeon_guide.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/mage/mage_source.tilesource\"\n"
-  "default_animation: \"dungeon_guide\"\n"
+  "default_animation: \"dungeon_guide_looking_left\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
--- passage/passage.go
+++ passage/passage.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/maps/map_source.tilesource\"\n"
-  "default_animation: \"hatch\"\n"
+  "default_animation: \"unlocked_door\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
