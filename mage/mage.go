b944a0cc98567b3cd2ffe7aa648875832415aac4
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-07-15 22:08:56 +0100

    * Added sprites and sprites logic to mage to be able to see range of teleport * Added binding for the space-bar on the keyboard so animation can be activated and deactivated

--- mage/mage.go
+++ mage/mage.go
@@ -103,3 +103,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "teleport_zone"
+  type: "sprite"
+  data: "tile_set: \"/mage/teleport_area.tilesource\"\n"
+  "default_animation: \"inactive_teleport\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
