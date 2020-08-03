504ff4f36bd6cf967d01a75adef2b2d12dc15b3e
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2020-08-03 22:06:39 +0100

    Completed dungeon learning final map Completed dungeon learning final game object Created skeleton for the game Fixed several bugs

--- skeleton/skeleton.go
+++ skeleton/skeleton.go
@@ -1,9 +1,24 @@
 components {
-  id: "map_level_5"
-  component: "/maps/tilemaps/map_level_5.tilemap"
+  id: "sprite"
+  component: "/skeleton/skeleton.sprite"
   position {
-    x: 80.0
-    y: 14.0
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/skeleton/skeleton.script"
+  position {
+    x: 0.0
+    y: 0.0
     z: 0.0
   }
   rotation {
@@ -21,14 +36,14 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"walls\"\n"
+  "group: \"mobs\"\n"
   "mask: \"mage\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: -43.0\n"
-  "      y: -8.0\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -40,66 +55,54 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: 59.0\n"
-  "      y: -8.0\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 3\n"
-  "    count: 3\n"
-  "  }\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: 8.0\n"
-  "      y: -106.0\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 6\n"
-  "    count: 3\n"
-  "  }\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: 7.0\n"
-  "      y: 100.0\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 9\n"
-  "    count: 3\n"
-  "  }\n"
-  "  data: 42.723\n"
-  "  data: 141.6685\n"
+  "  data: 7.912\n"
+  "  data: 7.8945\n"
   "  data: 10.0\n"
-  "  data: 42.723\n"
-  "  data: 142.2585\n"
-  "  data: 10.0\n"
-  "  data: 93.622\n"
-  "  data: 30.3305\n"
-  "  data: 10.4\n"
-  "  data: 93.622\n"
-  "  data: 30.3305\n"
-  "  data: 10.4\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "awareness"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"awareness\"\n"
+  "mask: \"mage\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 28.2735\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
